a47513f0753b0729e26795a31d788f7e907ecb2a
Patrick Logan <jpatricklogan@gmail.com>
2017-01-05 07:06:59 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,24 +5,22 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0,1,2], # Top row
-  [3,4,5], # Middle row
+  [0,1,2], #Top row
+  [3,4,5], #Middle row
   [6,7,8], #bottom row
-  [0,3,6], #left columns
+  [0,3,6], #left column
   [1,4,7], #Middle
-  [2,5,8], #right columns
+  [2,5,8], #right column
   [0,4,8], #diagonal
-  [2,4,6] #diagonal
+  [2,4,6]  #diagonal
 ]
 
 def won(board)
-    winner = board.include_in?(WIN_COMBINATIONS)
-  end
-  winner
+    board.include?(WIN_COMBINATIONS)
 end
 
 # def full
-# earn
+#
 # end
 #
 # def draw
